54f5fbb75ded6e69740d382f0446b57c63776614
dtrygoni <56765599+dtrygoni@users.noreply.github.com>
2023-11-09 13:34:39 +0200

    Add files via upload

--- Protein_check.py
+++ Protein_check.py
@@ -4,7 +4,7 @@
 import numpy as np
 import pandas as pd
 
-def Protein_check(df):
+def Protein_check(df,opt="other"):
     seqs=df['Sequence'].values
     ind=0
     std = list("ACDEFGHIKLMNPQRSTVWY")
@@ -13,10 +13,13 @@ def Protein_check(df):
             ind = i
             break
     if ind!=0:
-        string='Error : Unexpected character in protein sequence in position: '+str(ind)+' '+str(seq[0][ind])
+        string='Error : Unexpected character in protein sequence in position: '+str(ind)+' '+str(seqs[0][ind])
         return string
     else:
-        string='Correct format of the selected protein for the analysis. You can proceed to Parameters or Execute Tab panel'
+        if opt=="B_cell":
+            string='Correct format of the selected protein for the analysis. You can proceed either to Parameters Tab Panel to configure a custom analysis or you can proceed to  Execute panel from the menu to continue with the default options.'
+        else:
+            string='Correct format of the selected protein for the analysis. You can must proceed to Parameters Tab Panel in the menu to select the alleles for the epitope prediction.'
         return string
 
         
